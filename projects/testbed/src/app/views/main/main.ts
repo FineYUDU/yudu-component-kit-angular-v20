2a143b1d899f5afd98d247285a60f17d62d3945d
FineYudu <fine_567@hotmail.com>
2025-12-01 14:50:08 -0600

    yd-chip:component v1, the test still need to be completed

---
+++ projects/testbed/src/app/views/main/main.ts
@@ -0,0 +1,11 @@
+import { Component,} from '@angular/core';
+import { YdChip } from "../../components/yd-chip/yd-chip";
+
+@Component({
+  selector: 'app-main',
+  imports: [YdChip],
+  templateUrl: './main.html',
+  styleUrl: './main.css',
+})
+export class Main {
+}
