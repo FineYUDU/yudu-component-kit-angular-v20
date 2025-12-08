cd042f030e07e8c578714fbe0c75741a74182048
FineYudu <fine_567@hotmail.com>
2025-12-08 11:46:56 -0600

    chip finished

--- projects/yudu-component-kit/src/lib/components/yd-chip/yd-chip.spec.ts
+++ projects/yudu-component-kit/src/lib/components/yd-chip/yd-chip.spec.ts
@@ -3,6 +3,7 @@ import { provideZonelessChangeDetection } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { YdChip } from './yd-chip';
+
 describe('YdChip', () => {
   let fixture: ComponentFixture<YdChip>;
   let compiled:HTMLElement;
@@ -28,7 +29,51 @@ describe('YdChip', () => {
   });
   
   it('should create the app', () => {
-    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+  it('should initialize inputs with default values', ()=> {
+    expect(component.icon()).toBe(undefined);
+    expect(component.size()).toBe('normal');
+    expect(component.route()).toBe('./test');
+  });
+
+  it('should apply host default classes', () => {
+    expect(compiled.classList).toContain('yd-chip__container');
+  });
+
+  it('should initialize chip with class yd-chip__body', ()=> {
+    const chip = compiled.querySelector('a') as HTMLAnchorElement;
+
+    expect(chip).toBeTruthy();
+    expect(chip.classList).toContain('yd-chip__body');
+  });
+
+  it('should apply small to class', ()=> {
+    fixture.componentRef.setInput('size', 'small');
+
+    expect(component.size()).toBe('small');
+  });
+
+  it('should apply normal to class', ()=> {
+    fixture.componentRef.setInput('size', 'normal');
+
+    expect(component.size()).toBe('normal');
+  });
+
+  it('should apply large to class', ()=> {
+    fixture.componentRef.setInput('size', 'large');
+
+    expect(component.size()).toBe('large');
+  });
+
+  it('should apply address-book icon', ()=> {
+    fixture.componentRef.setInput('icon','address-book');
+    expect(component.icon()).toBe('address-book');
+  });
+
+  it('should change route to ./main', ()=> {
+    fixture.componentRef.setInput('route','./main');
+
+    expect(component.route()).toBe('./main');
   });
 });
